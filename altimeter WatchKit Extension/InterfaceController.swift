e5ac42cbc86d62fcae88bf060bedb8cfa4601272
Jason DuPertuis <gallahad@me.com>
2019-02-05 08:54:48 -0800

    Initial Commit

---
+++ altimeter WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  altimeter WatchKit Extension
+//
+//  Created by Jason DuPertuis on 2/5/19.
+//  Copyright © 2019 jdp. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
