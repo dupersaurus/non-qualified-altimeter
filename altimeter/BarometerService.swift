7db87ad722eb9194b51ef92af3c099ba1313e151
Jason DuPertuis <gallahad@me.com>
2019-02-05 20:59:55 -0800

    first

---
+++ altimeter/BarometerService.swift
@@ -0,0 +1,100 @@
+//
+//  BarometerService.swift
+//  altimeter
+//
+//  Created by Jason DuPertuis on 2/5/19.
+//  Copyright © 2019 jdp. All rights reserved.
+//
+
+import Foundation
+import CoreMotion
+
+enum BarometerError: Error {
+    case notAvailable
+}
+
+typealias AltitudeData = (qnh: Int, kpa: Double, inHg: Double, altitude: Double?, pressureAltitude: Double?)
+
+class BarometerService {
+    var qnh = 29.92
+    var lastKPA = 0.0;
+    
+    let altimeter = CMAltimeter()
+    
+    func observeBarometer(listener: @escaping (AltitudeData) -> Void) throws {
+        guard CMAltimeter.isRelativeAltitudeAvailable() else {
+            throw BarometerError.notAvailable
+        }
+        
+        
+        
+        altimeter.startRelativeAltitudeUpdates(to: .main) {
+            (data, error) in
+            
+            if data == nil {
+                print("No altimeter data available")
+            }
+            
+            else if error != nil {
+                print("\(String(describing: error))")
+            }
+            
+            else {
+                DispatchQueue.main.async {
+                    if let pressureKPA = data?.pressure.doubleValue {
+                        self.lastKPA = pressureKPA
+                        listener(self.calculateAltitudeComponents(pressureKPA: pressureKPA))
+                    } else {
+                        listener((self.qnhToInt(), 101.325, self.kpaToInhg(kpa: 101.325), nil, nil))
+                    }
+                }
+            }
+        }
+        
+        listener(calculateAltitudeComponents(pressureKPA: 101.325))
+    }
+    
+    private func calculateAltitudeComponents(pressureKPA: Double) -> AltitudeData {
+        let pressureHg = self.kpaToInhg(kpa: pressureKPA)
+        let altitude = self.calculateIndicatedAltitude(inHg: pressureHg)
+        let palt = self.calculatePressureAltitude(inHg: pressureHg)
+        
+        return (qnhToInt(), pressureKPA, pressureHg, altitude, palt)
+    }
+    
+    func setQNH(qnh: Int) -> AltitudeData {
+        self.qnh = Double(qnh) * 0.01;
+        return calculateAltitudeComponents(pressureKPA: lastKPA)
+    }
+    
+    func setIndicatedPressure(inHg: Int) -> AltitudeData {
+        let tmp = Double(inHg) / 100;
+        self.lastKPA = (tmp / 10) * 33.8639;
+        return calculateAltitudeComponents(pressureKPA: lastKPA)
+    }
+    
+    /// indicated altitude is 1000 ft per 1" different in Hg from QNH
+    private func calculateIndicatedAltitude(inHg: Double) -> Double {
+        return (self.qnh - inHg) * 1000;
+    }
+    
+    /// calculate the pressure altitude
+    private func calculatePressureAltitude(inHg: Double) -> Double {
+        let mb = inHg * 33.8639;
+        return 145366.45 * (1 - pow(mb / 1013.25, 0.190284));
+    }
+    
+    /// convert kilopascals to inches of mercury
+    private func kpaToInhg(kpa: Double) -> Double {
+        return kpa * 0.295300;
+    }
+    
+    // convert kilopascals to millibars
+    private func kpaToMb(kpa: Double) -> Double {
+        return kpa * 10;
+    }
+    
+    private func qnhToInt() -> Int {
+        return Int(qnh * 100)
+    }
+}
--- altimeter/ViewController.swift
+++ altimeter/ViewController.swift
@@ -6,15 +6,117 @@
 //  Copyright © 2019 jdp. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    @IBOutlet weak var kpaValue: UILabel!
+    @IBOutlet weak var inhgValue: UILabel!
+    @IBOutlet weak var qnhValue: UILabel!
+    @IBOutlet weak var altitudeValue: UILabel!
+    @IBOutlet weak var pressureValues: UIPickerView!
+    
+    let barometerService = BarometerService()
+    var pressureValueList: [Int] = []
+    var selectedPressureValue = 182;
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        pressureValues.delegate = self
+        pressureValues.dataSource = self
+        
+        var altSetting = 2810
+        while altSetting <= 3100 {
+            pressureValueList.append(altSetting)
+            altSetting += 1;
+        }
+        
+        pressureValues.selectRow(selectedPressureValue, inComponent: 0, animated: false)
+        
+        do {
+            try barometerService.observeBarometer() {
+                (data: AltitudeData) in
+                
+                self.applyAltitudeData(data: data)
+            }
+        } catch BarometerError.notAvailable {
+            print("Barometer not available")
+        } catch {
+            print("Unable to run barometer")
+        }
+    }
+    
+    func applyAltitudeData(data: AltitudeData) {
+        self.kpaValue.text = String(data.kpa)
+        self.qnhValue.text = inHgToString(inhg: data.qnh)
+        self.inhgValue.text = inHgToString(inhg: data.inHg)
+        
+        if let altitude = data.altitude {
+            self.altitudeValue.text = altitudeToString(altitude: altitude)
+        } else {
+            self.altitudeValue.text = "--"
+        }
     }
 
-
+    @IBAction func submitQNH(_ sender: Any) {
+        let altimeter = barometerService.setQNH(qnh: pressureValueList[selectedPressureValue])
+        applyAltitudeData(data: altimeter)
+    }
+    
+    @IBAction func submitDebugPressure(_ sender: Any) {
+        let altimeter = barometerService.setIndicatedPressure(inHg: pressureValueList[selectedPressureValue])
+        applyAltitudeData(data: altimeter)
+    }
+    
+    func convertPressureText(text: String) -> Double {
+        let scale: Int16 = 2
+        let behavior = NSDecimalNumberHandler(roundingMode: .plain, scale: scale, raiseOnExactness: false, raiseOnOverflow: false, raiseOnUnderflow: false, raiseOnDivideByZero: true)
+        
+        let dn = NSDecimalNumber(string: text).rounding(accordingToBehavior: behavior)
+        return Double(truncating: dn)
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pressureValueList.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return inHgToString(inhg: pressureValueList[row])
+        
+        //String(pressureValueList[row])
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedPressureValue = row;
+    }
+    
+    func inHgToString(inhg: Int) -> String {
+        return "\(inhg / 100).\(inhg % 100)"
+    }
+    
+    func inHgToString(inhg: Double) -> String {
+        return "\(Int(inhg)).\(Int(inhg * 100) % 100)"
+    }
+    
+    func altitudeToString(altitude: Double) -> String {
+        if (altitude < 0) {
+            return "0";
+        }
+        
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        return formatter.string(for: Int(altitude)) ?? "--"
+    }
 }
 
+extension Double {
+    func roundToDecimal(_ fractionDigits: Int) -> Double {
+        let multiplier = pow(10, Double(fractionDigits))
+        return Darwin.round(self * multiplier) / multiplier
+    }
+}
